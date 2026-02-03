5013fe2e3c727cf0bfb42adda6b00b511dc15cd3
KingSBeezy <KingSBeezy@DESKTOP-RB51KD4>
2026-02-03 23:10:39 +0200

    add comment

--- apitest/Program.cs
+++ apitest/Program.cs
@@ -13,8 +13,9 @@ namespace apitest
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            //Test comment
             var app = builder.Build();
+            
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
